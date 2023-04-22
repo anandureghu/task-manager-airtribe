aa32b0ef0818bba67609878d6c21e21c193ae9ff
anandureghu <anandukr145@gmail.com>
2023-04-22 11:37:10 +0530

    added internal server error exeption handling

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,41 +1,87 @@
+const httpStatus = require("http-status");
 const tasksService = require("../services/tasks.service");
 const data = require("../utils/data");
 
 const GetAllTasks = (req, res) => {
-  const params = {};
-  params.limit = req.query.limit ? +req.query.limit : data.tasks.length;
-  params.offset = req.query.offset ? +req.query.offset : 0;
-  params.status = req.query.status ? req.query.status : null;
-  params.sortBy = req.query.sortBy ? req.query.sortBy : "id";
-  params.sortOrder = req.query.sortOrder ? req.query.sortOrder : "asc";
+  try {
+    const params = {};
+    params.limit = req.query.limit ? +req.query.limit : data.tasks.length;
+    params.offset = req.query.offset ? +req.query.offset : 0;
+    params.status = req.query.status ? req.query.status : null;
+    params.sortBy = req.query.sortBy ? req.query.sortBy : "id";
+    params.sortOrder = req.query.sortOrder ? req.query.sortOrder : "asc";
 
-  const result = tasksService.getAllTasks(params);
-  res.status(result.code).send(result);
+    const result = tasksService.getAllTasks(params);
+    res.status(result.code).send(result);
+  } catch (error) {
+    const result = {
+      code: httpStatus.INTERNAL_SERVER_ERROR,
+      msg: "something happend please try again later",
+    };
+    console.error(error);
+    res.status(result.code).send(result);
+  }
 };
 
 const GetTask = (req, res) => {
-  const id = +req.params.taskId;
-  const result = tasksService.getTask(id);
-  res.status(result.code).send(result);
+  try {
+    const id = +req.params.taskId;
+    const result = tasksService.getTask(id);
+    res.status(result.code).send(result);
+  } catch (error) {
+    const result = {
+      code: httpStatus.INTERNAL_SERVER_ERROR,
+      msg: "something happend please try again later",
+    };
+    console.error(error);
+    res.status(result.code).send(result);
+  }
 };
 
 const CreateTask = (req, res) => {
-  const task = req.body;
-  const result = tasksService.createTask(task);
-  res.status(result.code).send(result);
+  try {
+    const task = req.body;
+    const result = tasksService.createTask(task);
+    res.status(result.code).send(result);
+  } catch (error) {
+    const result = {
+      code: httpStatus.INTERNAL_SERVER_ERROR,
+      msg: "something happend please try again later",
+    };
+    console.error(error);
+    res.status(result.code).send(result);
+  }
 };
 
 const UpdateTask = (req, res) => {
-  const id = +req.params.taskId;
-  const newTask = req.body.task;
-  const result = tasksService.updateTask(id, newTask);
-  res.status(result.code).send(result);
+  try {
+    const id = +req.params.taskId;
+    const newTask = req.body.task;
+    const result = tasksService.updateTask(id, newTask);
+    res.status(result.code).send(result);
+  } catch (error) {
+    const result = {
+      code: httpStatus.INTERNAL_SERVER_ERROR,
+      msg: "something happend please try again later",
+    };
+    console.error(error);
+    res.status(result.code).send(result);
+  }
 };
 
 const DeleteTask = (req, res) => {
-  const id = +req.params.taskId;
-  const result = tasksService.deleteTask(id);
-  res.status(result.code).send(result);
+  try {
+    const id = +req.params.taskId;
+    const result = tasksService.deleteTask(id);
+    res.status(result.code).send(result);
+  } catch (error) {
+    const result = {
+      code: httpStatus.INTERNAL_SERVER_ERROR,
+      msg: "something happend please try again later",
+    };
+    console.error(error);
+    res.status(result.code).send(result);
+  }
 };
 
 module.exports = {
